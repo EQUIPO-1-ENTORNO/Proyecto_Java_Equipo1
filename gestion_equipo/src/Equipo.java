b5456c2f173181498d2f2c615ca7a6eeab9220fa
Pablo <93923592+Pabloequisde@users.noreply.github.com>
2022-02-09 16:25:28 +0100

    categoria equipo ya puesto

--- gestion_equipo/src/Equipo.java
+++ gestion_equipo/src/Equipo.java
@@ -42,5 +42,33 @@ public class Equipo {
 	public int getRanking() {
 		return ranking;
 	}
+
+	public String categoriaEquipo(int ranking) {
+		
+		if(this.getNombreEquipo()!=null && this.getNombreEquipo()!="") {
+			
+			switch(ranking) {
+			
+			case 10:
+			case 9:
+			case 8:
+			case 7:
+				return "primera";
+			case 6:
+			case 5:
+			case 4:
+			case 3:
+				return "segunda";
+			case 2:
+			case 1:
+			case 0:
+				return "tercera";
+			default:
+				return null;
+			}
+		}else {
+			return null;	
+		}
+	}
 }
 
