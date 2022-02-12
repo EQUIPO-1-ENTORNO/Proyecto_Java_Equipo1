c203474daaa3e586e5bd7a711422db1a6c69f258
AdolfoSaladoLopez <93923566+AdolfoSaladoLopez@users.noreply.github.com>
2022-02-12 19:51:19 +0100

    AsignarEquipos refactorizado.

--- gestion_equipo/src/AsignarEquipo.java
+++ gestion_equipo/src/AsignarEquipo.java
@@ -1,41 +1,42 @@
 public class AsignarEquipo {
-
+	/* Atributos */
 	private Equipo equipo;
 	private Jugador jugador;
 
-
-
+	/* Constructor de la clase */
 	public AsignarEquipo() {
 
 	}
-
+	
+	/* Método para asignar equipo */
 	public void setEquipo(Equipo equipo) {
 
 		if (equipo != null) {
-			if ( equipo.getNombreEquipo() != null && equipo.getRanking() != 0) {
+			if (equipo.getNombreEquipo() != null 
+			    && equipo.getRanking() != 0) {
 
 				this.equipo = equipo;
-
-
+				
 			}  
 		}
-
-
 	}
 
+	/* Método para asignar jugador */
 	public void setJugador(Jugador jugador) {
-
-
+		/* Variables locales refactorizadas */
+		int edad = jugador.getEdad();
+		String nombre = jugador.getNombreJugador();
 		String idioma = jugador.getIdioma();
-
+		int edadIncorrecta = -1;
+		
 		if(jugador != null) {
-			if (jugador.getEdad() != -1 && jugador.getNombreJugador() != null && jugador.getIdioma() != null) {
+			if (edad != edadIncorrecta 
+			    && nombre != null 
+			    && idioma != null) {
 
 				this.jugador = jugador;
 
-			}
-
 		}
-
 	}
+}
 
