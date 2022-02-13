cc1ce511283a0ce801f9d614de9378cd79f6958d
Pablo <93923592+Pabloequisde@users.noreply.github.com>
2022-02-13 19:55:51 +0100

    equipo.java

--- gestion_equipo/src/Equipo.java
+++ gestion_equipo/src/Equipo.java
@@ -1,13 +1,14 @@
-
 public class Equipo {
+	/*Atributos*/
 	private String nombreEquipo;
 	private int ranking;
-	
-	
-	public Equipo () {
+
+	/*Constructor de la clase*/
+	public Equipo() {
+		
 	}
 	
-	
+	/*Método para asignar el Nombre del equipo*/
 	public void setNombreEquipo(String nombreEquipo) {
 		boolean correcto = true;
 		
@@ -16,10 +17,7 @@ public class Equipo {
 			if((letra >= 65 && letra<=90)||(letra >= 97 && letra<=122)||letra==32) {
 				//Es un caracter normal si ser alfanumerico y incluye los espacios
 				if (letra >= 97 && letra <= 122) { // letras minusculas
-					//if (i == 0) { // descomentar para cambiar solo la primera letra
 					nombreEquipo = nombreEquipo.substring(0, 1).toUpperCase() + nombreEquipo.substring(1);
-					//}
-					
 				}
 			}
 			else {
@@ -29,10 +27,8 @@ public class Equipo {
 		if(correcto&&(nombreEquipo.length()>=4&&nombreEquipo.length()<=20)) {
 			this.nombreEquipo=nombreEquipo;
 		}
-		
-		
-		
 	}
+	/*Método para asignar el Ranking del equipo*/
 	public void setRanking(int ranking) {
 		if(ranking>=0&&ranking<=10) {
 			this.ranking=ranking;
@@ -41,20 +37,20 @@ public class Equipo {
 		this.ranking=-1;
 		}
 	}
-	
+	/*Método para devolver el Nombre del equipo*/
 	public String getNombreEquipo() {
 		return nombreEquipo;
 	}
+	
+	/*Método para devolver el Ranking del equipo*/
 	public int getRanking() {
 		return ranking;
 	}
 
+	/*Metodo para devolver la Categoría del equipo*/
 	public String categoriaEquipo(int ranking) {
-		
 		if(this.getNombreEquipo()!=null && this.getNombreEquipo()!="") {
-			
 			switch(ranking) {
-			
 			case 10:
 			case 9:
 			case 8:
@@ -77,4 +73,3 @@ public class Equipo {
 		}
 	}
 }
-
